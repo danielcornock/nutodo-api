e33ed350bc96860a7996fc8c180225bde1c0475b
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-27 18:42:06 +0000

    add factories for controller and service

--- src/factories/controller.factory.ts
+++ src/factories/controller.factory.ts
@@ -1,10 +1,11 @@
 import { TodoController } from '../collections/todos/controller/todo.controller';
+import { ServiceFactory } from './service.factory';
 
 export class ControllerFactory {
   static create(controller: string): any {
     switch (controller) {
       case 'todo':
-        return new TodoController();
+        return new TodoController(ServiceFactory.create('todo'));
     }
   }
 }
