f47cd70d36cf8f296abe0568db53c2bf888f1b34
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-03-27 19:01:45 +0000

    update todo controller

--- src/collections/todos/controller/todo.controller.spec.ts
+++ src/collections/todos/controller/todo.controller.spec.ts
@@ -1,52 +1,61 @@
-import { TodoController } from './todo.controller';
-import { IReq, IRes, ReqStub, ResStub, StubCreator, ResponseFactory } from '@danielc7150/express-utils';
+import { IReq, IRes, ReqStub, ResponseFactory, ResponseFactoryStub, ResStub, StubCreator } from '@danielc7150/express-utils';
+
+import { TodoRequest } from '../data-objects/todo-request';
 import { TodoService } from '../service/todo.service';
 import { TodoServiceStub } from '../service/todo.service.stub';
+import { TodoController } from './todo.controller';
 
 describe('TodoController', () => {
-  let controller: TodoController, req: IReq, res: IRes, todoService: TodoService;
+  let controller: TodoController, req: IReq, res: IRes, todoService: TodoService, responseFactory: ResponseFactory;
 
   beforeEach(() => {
     req = StubCreator.create(ReqStub);
+    (req as any).user.id = 'userId';
     res = StubCreator.create(ResStub);
+    responseFactory = StubCreator.create(ResponseFactoryStub);
     todoService = StubCreator.create(TodoServiceStub);
+    jest.spyOn(ResponseFactory, 'create').mockReturnValue(responseFactory);
     controller = new TodoController(todoService);
   });
 
   describe('when getting all todos', () => {
+    let mockTodo: TodoRequest;
+
     beforeEach(() => {
-      (todoService.getAll as jest.Mock).mockResolvedValue([{ id: 1 }]);
-      jest.spyOn(ResponseFactory, 'successFind');
+      mockTodo = { category: 'week', completed: true, title: 'todo' };
+      (todoService.getAll as jest.Mock).mockResolvedValue([mockTodo]);
       controller.getAll(req, res);
     });
 
     it('should fetch the todos from the service', () => {
-      expect(todoService.getAll).toHaveBeenCalledWith();
+      expect(todoService.getAll).toHaveBeenCalledWith('userId');
     });
 
     it('should return the todos', () => {
-      expect(ResponseFactory.successFind).toHaveBeenCalledWith(res, {
-        data: { todos: [{ id: 1 }] },
+      expect(responseFactory.successFind).toHaveBeenCalledWith(res, {
+        data: { todos: [mockTodo] },
         name: 'todos'
       });
     });
   });
 
   describe('when creating a todo', () => {
+    let mockTodo: TodoRequest;
+
     beforeEach(() => {
-      (todoService.create as jest.Mock).mockResolvedValue({ id: 2 });
-      jest.spyOn(ResponseFactory, 'successCreate');
-      req.body = { id: 2 };
+      mockTodo = { category: 'week', completed: true, title: 'todo' };
+      (todoService.create as jest.Mock).mockResolvedValue(mockTodo);
+      req.body = mockTodo;
       controller.create(req, res);
     });
 
     it('should create the new todo', () => {
-      expect(todoService.create).toHaveBeenCalledWith(req.body);
+      expect(todoService.create).toHaveBeenCalledWith(req.body, 'userId');
     });
 
     it('should return the created todo', () => {
-      expect(ResponseFactory.successCreate).toHaveBeenCalledWith(res, {
-        data: { todo: { id: 2 } },
+      expect(responseFactory.successCreate).toHaveBeenCalledWith(res, {
+        data: { todo: mockTodo },
         name: 'todo'
       });
     });
@@ -55,17 +64,16 @@ describe('TodoController', () => {
   describe('when getting one todo', () => {
     beforeEach(() => {
       (todoService.getOne as jest.Mock).mockResolvedValue('todo');
-      jest.spyOn(ResponseFactory, 'successFind');
       req.params.id = 'id';
       controller.getOne(req, res);
     });
 
     it('should fetch the todo', () => {
-      expect(todoService.getOne).toHaveBeenCalledWith('id');
+      expect(todoService.getOne).toHaveBeenCalledWith('id', 'userId');
     });
 
     it('should return the found todo', () => {
-      expect(ResponseFactory.successFind).toHaveBeenCalledWith(res, {
+      expect(responseFactory.successFind).toHaveBeenCalledWith(res, {
         data: { todo: 'todo' },
         name: 'todo'
       });
@@ -75,34 +83,32 @@ describe('TodoController', () => {
   describe('when deleting a todo', () => {
     beforeEach(() => {
       (todoService.deleteOne as jest.Mock).mockResolvedValue(null);
-      jest.spyOn(ResponseFactory, 'successDelete');
       req.params.id = 'id';
       controller.deleteOne(req, res);
     });
 
     it('should delete the todo', () => {
-      expect(todoService.deleteOne).toHaveBeenCalledWith('id');
+      expect(todoService.deleteOne).toHaveBeenCalledWith('id', 'userId');
     });
 
     it('should respond with a 204', () => {
-      expect(ResponseFactory.successDelete).toHaveBeenCalledWith(res);
+      expect(responseFactory.successDelete).toHaveBeenCalledWith(res);
     });
   });
 
   describe('when updating a todo', () => {
     beforeEach(() => {
       (todoService.update as jest.Mock).mockResolvedValue('todo');
-      jest.spyOn(ResponseFactory, 'successCreate');
       req.params.id = 'id';
       controller.update(req, res);
     });
 
     it('should update the todo', () => {
-      expect(todoService.update).toHaveBeenCalledWith('id', req.body);
+      expect(todoService.update).toHaveBeenCalledWith('id', req.body, 'userId');
     });
 
     it('should return the created todo', () => {
-      expect(ResponseFactory.successCreate).toHaveBeenCalledWith(res, {
+      expect(responseFactory.successCreate).toHaveBeenCalledWith(res, {
         data: { todo: 'todo' },
         name: 'todo'
       });
--- src/collections/todos/controller/todo.controller.ts
+++ src/collections/todos/controller/todo.controller.ts
@@ -1,50 +1,66 @@
 import { IReq, IRes, ResponseFactory } from '@danielc7150/express-utils';
-import { TodoService } from '../service/todo.service';
+
 import { TODO_DOCUMENT_NAMES } from '../constants/todo-document-names.constant';
-import { ITodo } from '../interfaces/todo.interface';
+import { TodoRequest } from '../data-objects/todo-request';
+import { TodoResponse } from '../data-objects/todo-response';
+import { todoTemplate } from '../data-objects/todo-template';
+import { TodoService } from '../service/todo.service';
 
 export class TodoController {
   private readonly _todoService: TodoService;
+  private readonly _responseFactory: ResponseFactory;
 
   constructor(todoService: TodoService) {
     this._todoService = todoService;
+    this._responseFactory = ResponseFactory.create(TODO_DOCUMENT_NAMES.plural);
   }
 
-  public async getAll(_req: IReq, res: IRes): Promise<void> {
-    const todos: any = await this._todoService.getAll();
+  public async getAll(req: IReq, res: IRes): Promise<void> {
+    const todos: Array<TodoResponse> = await this._todoService.getAll(req.user.id);
 
-    ResponseFactory.successFind(res, {
+    this._responseFactory.successFind(res, {
       data: { todos },
       name: TODO_DOCUMENT_NAMES.plural
     });
   }
 
   public async create(req: IReq, res: IRes): Promise<void> {
-    const todo: any = await this._todoService.create(req.body);
-    ResponseFactory.successCreate(res, {
+    const reqTodo: TodoRequest = new TodoRequest(req.body);
+    const todo: TodoResponse = await this._todoService.create(reqTodo, req.user.id);
+
+    this._responseFactory.successCreate(res, {
       data: { todo },
       name: TODO_DOCUMENT_NAMES.singular
     });
   }
 
   public async update(req: IReq, res: IRes): Promise<void> {
-    const todo: ITodo = await this._todoService.update(req.params.id, req.body);
-    ResponseFactory.successCreate(res, {
+    const todoUpdate: TodoRequest = new TodoRequest(req.body);
+    const todo: TodoResponse = await this._todoService.update(req.params.id, todoUpdate, req.user.id);
+    this._responseFactory.successCreate(res, {
       data: { todo },
       name: TODO_DOCUMENT_NAMES.singular
     });
   }
 
   public async getOne(req: IReq, res: IRes): Promise<void> {
-    const todo: ITodo = await this._todoService.getOne(req.params.id);
-    ResponseFactory.successFind(res, {
+    const todo: TodoResponse = await this._todoService.getOne(req.params.id, req.user.id);
+    this._responseFactory.successFind(res, {
       data: { todo },
       name: TODO_DOCUMENT_NAMES.singular
     });
   }
 
   public async deleteOne(req: IReq, res: IRes): Promise<void> {
-    await this._todoService.deleteOne(req.params.id);
-    ResponseFactory.successDelete(res);
+    await this._todoService.deleteOne(req.params.id, req.user.id);
+    this._responseFactory.successDelete(res);
+  }
+
+  public async getTemplate(_req: IReq, res: IRes): Promise<void> {
+    this._responseFactory.successFind(res, {
+      data: { todoTemplate: todoTemplate as any },
+      name: 'todoTemplate',
+      selfLinkOverride: 'todos'
+    });
   }
 }
